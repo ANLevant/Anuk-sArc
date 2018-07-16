a2d4d8af138fda0252c8f85d366e04f0c7a6c9f1
ANLevant <levant.alejandro@hotmail.com>
2018-07-16 01:03:06 -0500

    Initial commit with changes on .gitignore

---
+++ Source/AnuksArcPrototype/GakalState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "Gakal.GakalState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANUKSARCPROTOTYPE_API AGakalState : public APlayerState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/AnuksArcPrototype/LanguageService.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LanguageService.h"
+
+LanguageService::LanguageService()
+{
+}
+
+LanguageService::~LanguageService()
+{
+}
